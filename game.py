56c57f9fea166ade361dbddcb642ba037ce83130
Kevin Levasseur <kevin_levasseur@telus.net>
2024-08-22 17:12:56 -0700

    feat: can walk around town and field

--- game.py
+++ game.py
@@ -1,13 +1,38 @@
-import character
+import character, locations
 
 print("Welcome to Simple RPG! Please tell us your name before we begin...")
 
 userInputName = input("Enter your character's name: ")
 
-player1 = character.NewPlayerCharacter(userInputName)
+player = character.NewPlayerCharacter(userInputName)
 
-player1.display_stats()
+player.display_stats()
 
-player1.class_change()
+location = locations.TownOfRespite
 
-player1.display_stats()
+while player.health > 0:
+  while location == locations.TownOfRespite:
+    print(f"You are walking around the {location.name}. What would you like to do?")
+    userInputAction = input("Actions: explore, go to fields\nType your action: ")
+    if userInputAction == "explore":
+      print("You explore a bit")
+      location.explore(location)
+    elif userInputAction == "go to fields":
+      print("You leave town and travel towards the Fields of Beginning")
+      location = locations.FieldsOfBeginning
+    else:
+      print("You can't do that!")
+  
+  while location == locations.FieldsOfBeginning:
+    print(f"You are standing in the {location.name}, what would you like to do?")
+    userInputAction = input("Actions: explore, go to town\nType your action: ")
+    if userInputAction == "explore":
+      print("You walk around the fields a bit")
+      location.explore(location)
+    elif userInputAction == "go to town":
+      print("you head into town")
+      location = locations.TownOfRespite
+    else:
+      print("You can't do that!")
+
+print("your quest is over")
