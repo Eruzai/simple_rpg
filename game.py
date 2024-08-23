0cc2b506e703fb299998f3ad52fb3f360ed1574c
Kevin Levasseur <kevin_levasseur@telus.net>
2024-08-23 16:58:34 -0700

    feat: player can attack enemies with physical or magic, inspect enemy stats, run from enemies, and rest in town

--- game.py
+++ game.py
@@ -9,15 +9,40 @@ player = character.NewPlayerCharacter(userInputName)
 player.display_stats()
 
 location = locations.TownOfRespite()
-fighting = False
-treasure = False
 
 while player.health > 0:
   print(f"You are walking around the {location.name}.\nActions:\n  'e' to explore\n  'l' to leave")
+  if location.name == "Town of Respite":
+    print("  'r' - to rest at an inn")
   userInputAction = input("What do you do? -> ")
   if userInputAction == "e":
     print("You explore a bit")
     location.explore()
+    while location.enemy and location.enemy.health > 0 and player.health > 0:
+      print("Actions:\n  'a' - attack\n  'm' - magical attack\n  'i' - inspect enemy\n  'r' - run away")
+      userInputAction = input("What do you do? -> ")
+      if userInputAction == "a":
+        damage = player.strength
+        if hasattr(location.enemy, 'physicalDef'):
+          enemyDefMultiplier = location.enemy.physicalDef / 100
+          damage = player.strength - player.strength * enemyDefMultiplier // 1
+        location.enemy.damage_taken(damage)
+        print(f"You inflict {damage} points of physical damage to {location.enemy.name}")
+      elif userInputAction == "m":
+        damage = player.intellect
+        if hasattr(location.enemy, 'magicDef'):
+          enemyDefMultiplier = location.enemy.magicDef / 100
+          damage = player.intellect - player.intellect * enemyDefMultiplier // 1
+        location.enemy.damage_taken(damage)
+        print(f"You inflict {damage} points of magical damage to {location.enemy.name}")
+      elif userInputAction == "i":
+        location.enemy.display_stats()
+      elif userInputAction == "r":
+        print(f"You run away from {location.enemy.name}")
+        location.enemy = None
+      else:
+        print("That's not a valid action")
+    
   elif userInputAction == "l":
     print("You open your map and find a few interesting locations nearby...\nLocations:\n  't' - Town of Respite\n  'f' - Fields of Beginning (levels 1 - 5)\n  'm' - Middling Marrow (levels 6 - 10)\n  'k' - Keep of the End (levels 11 - 15)")
     userInputLocation = input("Where would you like to go? -> ")
@@ -35,6 +60,9 @@ while player.health > 0:
       location = locations.KeepOfTheEnd()
     else:
       print("That place doesn't exist!")
+  elif userInputAction == "r":
+    print("You rest at the inn.\nYour health and magic are restored!")
+    player.rest()
   else:
     print("You can't do that!")
 
