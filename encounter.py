c7b9da50c02d826204d58dcd9f972e775a6ddf54
Kevin Levasseur <kevin_levasseur@telus.net>
2024-08-26 10:41:15 -0700

    feat: refactor: moved experience gain logic from game file, simplified attack logic, added enemy attack, added game over logic

--- encounter.py
+++ encounter.py
@@ -4,29 +4,19 @@ class Fight:
     pass
   
   def battle(enemy, player):
-    while enemy and enemy.health > 0 and player.health > 0:
-      print("Actions:\n  'a' - attack\n  'm' - magical attack\n  'i' - inspect enemy\n  'r' - run away")
+    while enemy:
+      print("Actions:\n  'a' - physical attack\n  'm' - magical attack\n  'i' - inspect enemy\n  'r' - run away")
       userInputAction = input("What do you do? -> ")
 
       if userInputAction == "a":
         damage = player.strength
-
-        if hasattr(enemy, 'physicalDef'):
-          enemyDefMultiplier = enemy.physicalDef / 100
-          damage = player.strength - player.strength * enemyDefMultiplier // 1
-
-        enemy.damage_taken(damage)
-        print(f"You inflict {damage} points of physical damage to {enemy.name}")
+        print(f"You attempt to attack {enemy.name} with a physical attack!")
+        enemy.damage_taken(damage, "physical")
 
       elif userInputAction == "m":
         damage = player.intellect
-
-        if hasattr(enemy, 'magicDef'):
-          enemyDefMultiplier = enemy.magicDef / 100
-          damage = player.intellect - player.intellect * enemyDefMultiplier // 1
-
-        enemy.damage_taken(damage)
-        print(f"You inflict {damage} points of magical damage to {enemy.name}")
+        print(f"You attempt to attack {enemy.name} with a magical attack!")
+        enemy.damage_taken(damage, "magical")
 
       elif userInputAction == "i":
         enemy.display_stats()
@@ -34,6 +24,29 @@ class Fight:
       elif userInputAction == "r":
         print(f"You run away from {enemy.name}")
         enemy = None
+        break
 
       else:
         print("That's not a valid action")
+
+      if enemy.health <= 0:
+        print(f"{enemy.name} has been vanquished!")
+        print(f"You gain {enemy.experience} experience points!")
+        player.experience += enemy.experience
+        enemy = None
+
+        while player.experienceNeeded <= player.experience:
+          player.level_up()
+
+          if player.job == "Novice" and player.level == 5:
+            player.class_change()
+        break
+      
+      damage = 10
+      attackType = "physical"
+      player.damage_taken(damage, attackType)
+      print(f"{enemy.name} attacks you with a {attackType} attack!\nYou take {damage} points of damage!")
+
+      if player.health <= 0:
+        print(f"{player.name} has been defeated! Oh no!")
+        break
