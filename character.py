f1558fed87db23834668f58c94acfbea1f7c7f1e
Kevin Levasseur <kevin_levasseur@telus.net>
2024-08-29 18:35:31 -0700

    feat: stats have base which are multiplied by job multipliers for that job, jobs can be swapped and unlocked

--- character.py
+++ character.py
@@ -1,26 +1,46 @@
-from random import randint, choice
+from random import randint
+import jobs
 
 class NewPlayerCharacter:
   def __init__(self, name):
     self.name = name
-    self.job = "Novice"
+    self.unlockedJobs = {"Novice": jobs.Novice()}
+    self.job = self.unlockedJobs["Novice"]
     self.level = 1
     self.experience = 0
     self.experienceNeeded = 100
-    self.maxHealth = randint(40, 60)
-    self.maxMagic = randint(10, 15)
-    self.health = self.maxHealth
-    self.magic = self.maxMagic
-    self.strength = randint(10, 15)
-    self.intellect = randint(10, 15)
-    self.physicalDef = 0
-    self.magicDef = 0
+    self.baseMaxHealth = randint(40, 60)
+    self.baseMaxMagic = randint(10, 15)
+    self.baseStrength = randint(10, 15)
+    self.baseIntellect = randint(10, 15)
+    self.basePhysicalDef = 0
+    self.baseMagicDef = 0
     self.head = None
     self.body = None
     self.weapon = None
     self.legs = None
     self.feet = None
     self.ring = None
+    self.maxHealth = self.baseMaxHealth
+    self.maxMagic = self.baseMaxMagic
+    self.health = self.maxHealth
+    self.magic = self.maxMagic
+    self.strength = self.baseStrength
+    self.intellect = self.baseIntellect
+    self.physicalDef = 0
+    self.magicDef = 0
+
+  def calculate_stats(self):
+    self.maxHealth = self.baseMaxHealth * self.job.healthMultiplier // 1
+    self.maxMagic = self.baseMaxMagic * self.job.magicMultiplier // 1
+    self.strength = self.baseStrength * self.job.strengthMultiplier // 1
+    self.intellect = self.baseIntellect * self.job.intellectMultiplier // 1
+    self.physicalDef = self.basePhysicalDef + self.job.physicalDef
+    self.magicDef = self.baseMagicDef + self.job.magicDef
+    if self.health > self.maxHealth:
+      self.health = self.maxHealth
+    if self.magic > self.maxMagic:
+      self.magic = self.maxMagic
   
   def damage_taken(self, basedamage, attackType):
     damage = basedamage
@@ -31,70 +51,10 @@ class NewPlayerCharacter:
       multiplier = self.magicDef / 100
     damage -= basedamage * multiplier // 1
     self.health -= damage
-    
-  def rest(self):
-    self.health = self.maxHealth
-    self.magic = self.maxMagic
-  
-  def level_up(self):
-    self.level += 1
-    self.experienceNeeded += 100 + self.experienceNeeded * 0.25 // 1
-    if self.job == "Novice":
-      self.maxHealth += randint(5, 10)
-      self.maxMagic += randint(1, 3)
-      self.health = self.maxHealth
-      self.magic = self.maxMagic
-      self.strength += randint(1, 3)
-      self.intellect += randint(1, 3)
-    elif self.job == "Warrior":
-      self.maxHealth += randint(12, 25)
-      self.maxMagic += choice([0, 0, 0, 1, 2])
-      self.health = self.maxHealth
-      self.magic = self.maxMagic
-      self.strength += randint(3, 5)
-      self.intellect += choice([0, 0, 0, 1, 2])
-      self.physicalDef += choice([0, 0, 0, 0, 1, 2, 3])
-      self.magicDef += choice([0, 0, 0, 1])
-    elif self.job == "Wizard":
-      self.maxHealth += randint(12, 18)
-      self.maxMagic += randint(4, 7)
-      self.health = self.maxHealth
-      self.magic = self.maxMagic
-      self.strength += choice([0, 0, 0, 1, 2])
-      self.intellect += randint(4, 8)
-      self.physicalDef += choice([0, 0, 0, 1])
-      self.magicDef += choice([0, 0, 0, 0, 1, 2, 3])
-    
-    self.display_stats()
-  
-  def class_change(self):
-    userSelectedClass = input("What class will you choose? Warrior or Wizard?\nType War for Warrior or Wiz for Wizard! -> ")
-    if userSelectedClass == "War":
-      self.job = "Warrior"
-      self.maxHealth += randint(25, 40)
-      self.health = self.maxHealth
-      self.strength += randint(5, 15)
-      self.physicalDef = randint(5, 15)
-      self.magicDef = randint(0, 5)
-      print(f"{self.name} Has changed his job to Warrior!\n")
-      self.display_stats()
-    elif userSelectedClass == "Wiz":
-      self.job = "Wizard"
-      self.maxHealth += randint(15, 30)
-      self.maxMagic += randint(15, 25)
-      self.health = self.maxHealth
-      self.magic = self.maxMagic
-      self.intellect += randint(8, 17)
-      self.physicalDef = randint(0, 5)
-      self.magicDef = randint(5, 15)
-      print(f"{self.name} Has changed his job to Wizard!\n")
-      self.display_stats()
-    else:
-      print("I don't understand... try again!\n")
-      self.class_change()
-  
+    print(f"You take {damage} points of damage!\n")
+
   def display_stats(self):
-    print(f"{self.name} the {self.job}'s stats!")
+    print(f"{self.name} the {self.job.name}'s stats!")
     print(f"Level: {self.level}")
     print(f"Experience to next Level up: {self.experienceNeeded - self.experience}")
     print(f"Health: {self.health}/{self.maxHealth}")
@@ -105,10 +65,35 @@ class NewPlayerCharacter:
     print(f"Magic Defense: {self.magicDef}\n")
   
   def display_equipment(self):
-    print(f"{self.name} the {self.job}'s equipment!")
+    print(f"{self.name} the {self.job.name}'s equipment!")
     print(f"Head: {self.head}")
     print(f"Body: {self.body}")
     print(f"Weapon: {self.weapon}")
     print(f"Legs: {self.legs}")
     print(f"Feet: {self.feet}")
     print(f"Ring: {self.ring}\n")
+
+  def rest(self):
+    self.health = self.maxHealth
+    self.magic = self.maxMagic
+
+  def unlock_job(self, jobObject, jobName):
+    self.unlockedJobs[jobName] = jobObject()
+  
+  def switch_job(self, jobName):
+    self.job = self.unlockedJobs[jobName]
+    self.calculate_stats()
+    self.display_stats()
+
+  def level_up(self):
+    self.level += 1
+    self.experienceNeeded += 100 + self.experienceNeeded * 0.25 // 1
+    self.baseMaxHealth += randint(5, 10)
+    self.baseMaxMagic += randint(1, 3)
+    self.baseHealth = self.maxHealth
+    self.baseMagic = self.maxMagic
+    self.baseStrength += randint(1, 3)
+    self.baseIntellect += randint(1, 3)
+    self.calculate_stats()
+    self.rest()
+    self.display_stats()
