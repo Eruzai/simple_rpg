4572197f12fe4997ed0ae22f631e58de1d7bbf3a
Kevin Levasseur <kevin_levasseur@telus.net>
2024-09-13 11:00:59 -0700

    feat: more items for testing purposes

--- equipment.py
+++ equipment.py
@@ -11,14 +11,31 @@ class Item:
     for key, value in self.stats.items():
       print(f"  {key} by {value}")
 
-class RingOfStats(Item):
+class Ring(Item):
   def __init__(self):
     super().__init__()
     self.name = "Ring of Stats"
     self.equipSlot = "ring"
-    self.stats["health"] = 150
+    self.stats["maxHealth"] = 150
+    self.stats["maxMagic"] = 50
     self.stats["strength"] = 50
     self.stats["intellect"] = 50
-    self.stats["magic"] = 50
     self.stats["physicalDef"] = 15
+    self.stats["magicDef"] = 15
+
+class Loop(Item):
+  def __init__(self):
+    super().__init__()
+    self.name = "Loop of Stats"
+    self.equipSlot = "ring"
+    self.stats["maxHealth"] = 200
+    self.stats["maxMagic"] = 100
+
+class Robe(Item):
+  def __init__(self):
+    super().__init__()
+    self.name = "Robe of Stats"
+    self.equipSlot = "body"
+    self.stats["maxMagic"] = 50
+    self.stats["intellect"] = 50
     self.stats["magicDef"] = 15
