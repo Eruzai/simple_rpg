5978b56de3f56285c58a4f0b43e172a55f03a3df
Kevin Levasseur <kevin_levasseur@telus.net>
2024-09-13 14:38:20 -0700

    chore: test equipment has random stats

--- equipment.py
+++ equipment.py
@@ -16,61 +16,61 @@ class Ring(Item):
     super().__init__()
     self.name = "Ring of Stats"
     self.equipSlot = "ring"
-    self.stats["maxHealth"] = 150
-    self.stats["maxMagic"] = 50
-    self.stats["strength"] = 50
-    self.stats["intellect"] = 50
-    self.stats["physicalDef"] = 15
-    self.stats["magicDef"] = 15
+    self.stats["maxHealth"] = randint(1, 10)
+    self.stats["maxMagic"] = randint(1, 10)
+    self.stats["strength"] = randint(1, 10)
+    self.stats["intellect"] = randint(1, 10)
+    self.stats["physicalDef"] = randint(1, 10)
+    self.stats["magicDef"] = randint(1, 10)
 
 class Loop(Item):
   def __init__(self):
     super().__init__()
     self.name = "Loop of Stats"
     self.equipSlot = "ring"
-    self.stats["maxHealth"] = 200
-    self.stats["maxMagic"] = 100
+    self.stats["maxHealth"] = randint(1, 10)
+    self.stats["maxMagic"] = randint(1, 10)
 
 class Robe(Item):
   def __init__(self):
     super().__init__()
     self.name = "Robe of Stats"
     self.equipSlot = "body"
-    self.stats["maxMagic"] = 50
-    self.stats["intellect"] = 50
-    self.stats["magicDef"] = 15
+    self.stats["maxMagic"] = randint(1, 10)
+    self.stats["intellect"] = randint(1, 10)
+    self.stats["magicDef"] = randint(1, 10)
 
 class Pants(Item):
   def __init__(self):
     super().__init__()
     self.name = "Pants of Stats"
     self.equipSlot = "legs"
-    self.stats["maxHealth"] = 50
-    self.stats["strength"] = 5
-    self.stats["physicalDef"] = 5
+    self.stats["maxHealth"] = randint(1, 10)
+    self.stats["strength"] = randint(1, 10)
+    self.stats["physicalDef"] = randint(1, 10)
 
 class Shoes(Item):
   def __init__(self):
     super().__init__()
     self.name = "Shoes of Stats"
     self.equipSlot = "feet"
-    self.stats["maxHealth"] = 15
-    self.stats["maxMagic"] = 5
-    self.stats["intellect"] = 50
-    self.stats["magicDef"] = 5
+    self.stats["maxHealth"] = randint(1, 10)
+    self.stats["maxMagic"] = randint(1, 10)
+    self.stats["intellect"] = randint(1, 10)
+    self.stats["magicDef"] = randint(1, 10)
 
 class Hat(Item):
   def __init__(self):
     super().__init__()
     self.name = "Hat of Stats"
     self.equipSlot = "head"
-    self.stats["intellect"] = 5
-    self.stats["magicDef"] = 5
+    self.stats["intellect"] = randint(1, 10)
+    self.stats["magicDef"] = randint(1, 10)
 
 class Sword(Item):
   def __init__(self):
     super().__init__()
     self.name = "Sword of Stats"
     self.equipSlot = "weapon"
-    self.stats["strength"] = 5
-    self.stats["intellect"] = 5
+    self.stats["strength"] = randint(1, 10)
+    self.stats["intellect"] = randint(1, 10)
