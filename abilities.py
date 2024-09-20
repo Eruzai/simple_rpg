dcce3eaa51f4f81fafa6c6a6fba4b8563e2e1723
Kevin Levasseur <kevin_levasseur@telus.net>
2024-09-19 20:19:11 -0700

    feat: abilities have costs, all jobs have abilties

--- abilities.py
+++ abilities.py
@@ -73,6 +73,7 @@ class Attack:
     self.name = "Attack"
     self.numberOfAttacks = 1
     self.multiplier = 1
+    self.abilityCost = 0
   
   def execute(self, encounter, player):
     attackPower = player.strength
@@ -88,7 +89,7 @@ class MagicBolt:
   def execute(self, encounter, player):
     attackPower = player.intellect
     AttackMethods.single_target_attack(self, encounter, self.name, "magical", attackPower, self.numberOfAttacks, self.multiplier)
-    player.magic -= self.abilityCost
+    player.abilityPoints -= self.abilityCost
 
 class LuckyStrike:
   def __init__(self):
@@ -101,7 +102,7 @@ class LuckyStrike:
     attackPower = choice([player.strength, player.intellect])
     attackType = choice(["physical", "magical"])
     AttackMethods.single_target_attack(self, encounter, self.name, attackType, attackPower, self.numberOfAttacks, self.multiplier)
-    player.magic -= self.abilityCost
+    player.abilityPoints -= self.abilityCost
 
 class DoubleStrike:
   def __init__(self):
@@ -113,7 +114,7 @@ class DoubleStrike:
   def execute(self, encounter, player):
     attackPower = player.strength
     AttackMethods.single_target_attack(self, encounter, self.name, "physical", attackPower, self.numberOfAttacks, self.multiplier)
-    player.magic -= self.abilityCost
+    player.abilityPoints -= self.abilityCost
 
 class QuickCast:
   def __init__(self):
@@ -125,7 +126,7 @@ class QuickCast:
     attackPower = player.intellect
     numberOfAttacks = randint(2, 5)
     AttackMethods.random_targets_attack(self, encounter, self.name, "magical", attackPower, numberOfAttacks, self.multiplier)
-    player.magic -= self.abilityCost
+    player.abilityPoints -= self.abilityCost
 
 class MagicBlast:
   def __init__(self):
@@ -138,6 +139,7 @@ class MagicBlast:
   def execute(self, encounter, player):
     attackPower = player.intellect
     AttackMethods.random_secondary_target_attack(self, encounter, self.name, "magical", attackPower, self.numberOfAttacks, self.multiplier, self.splashMultiplier)
+    player.abilityPoints -= self.abilityCost
 
 class FocusMagic:
   def __init__(self):
@@ -149,62 +151,210 @@ class FocusMagic:
     attackPower = player.intellect
     numberOfAttacks = randint(1, 3)
     AttackMethods.single_target_attack(self, encounter, self.name, "magical", attackPower, numberOfAttacks, self.multiplier)
-    player.magic -= self.abilityCost
+    player.abilityPoints -= self.abilityCost
 
 class RapidStrikes:
   def __init__(self):
     self.name = "Rapid Strikes"
     self.multiplier = 0.4
+    self.abilityCost = 5
   
   def execute(self, encounter, player):
     attackPower = player.strength
     numberOfAttacks = randint(2, 5)
     AttackMethods.single_target_attack(self, encounter, self.name, "physical", attackPower, numberOfAttacks, self.multiplier)
+    player.abilityPoints -= self.abilityCost
 
 class WildStrikes:
   def __init__(self):
     self.name = "Wild Strikes"
     self.multiplier = 0.3
     self.splashMultiplier = 0.15
+    self.abilityCost = 5
   
   def execute(self, encounter, player):
     attackPower = player.strength
     numberOfAttacks = randint(2, 5)
     AttackMethods.random_secondary_target_attack(self, encounter, self.name, "physical", attackPower, numberOfAttacks, self.multiplier, self.splashMultiplier)
+    player.abilityPoints -= self.abilityCost
 
 class FocusStrike:
   def __init__(self):
     self.name = "Focus Strike"
     self.multiplier = 1.5
     self.numberOfAttacks = 1
+    self.abilityCost = 7
   
   def execute(self, encounter, player):
     attackPower = player.strength
     AttackMethods.single_target_attack(self, encounter, self.name, "physical", attackPower, self.numberOfAttacks, self.multiplier)
-
-class Cleave:
-  def __init__(self):
-    self.name = "Cleave"
-    self.attackType = "physical"
-
-  def execute(self, encounter, player):
-    attackPower = player.strength
-    AttackMethods.random_secondary_target_attack(self, encounter, self.name, "physical", attackPower, 1, 0.8, 0.5)
+    player.abilityPoints -= self.abilityCost
 
 class ChaoticMagic:
   def __init__(self):
     self.name = "Chaotic Magic"
-    self.attackType = "magical"
+    self.multiplier = 0.3
+    self.abilityCost = 7
   
   def execute(self, encounter, player):
     attackPower = player.intellect
-    AttackMethods.random_targets_attack(self, encounter, self.name, self.attackType, attackPower, 5, 0.6)
+    numberOfAttacks = randint(3, 7)
+    AttackMethods.random_targets_attack(self, encounter, self.name, "magical", attackPower, numberOfAttacks, self.multiplier)
+    player.abilityPoints -= self.abilityCost
 
-class Explosion:
+class ArcaneBolt:
   def __init__(self):
-    self.name = "Explosion"
-    self.attackType = "magical"
+    self.name = "Arcane Bolt"
+    self.multiplier = 0.4
+    self.splashMultiplier = 0.2
+    self.numberOfAttacks = 3
+    self.abilityCost = 10
   
   def execute(self, encounter, player):
     attackPower = player.intellect
-    AttackMethods.all_target_attack(self, encounter, self.name, self.attackType, attackPower, 1, 1.5, 0.75)
+    AttackMethods.random_secondary_target_attack(self, encounter, self.name, "magical", attackPower, self.numberOfAttacks, self.multiplier, self.splashMultiplier)
+    player.abilityPoints -= self.abilityCost
+
+class DarkBolt:
+  def __init__(self):
+    self.name = "Dark Bolt"
+    self.multiplier = 2
+    self.splashMultiplier = 0.6
+    self.numberOfAttacks = 1
+    self.abilityCost = 10
+  
+  def execute(self, encounter, player):
+    attackPower = player.intellect
+    AttackMethods.all_target_attack(self, encounter, self.name, "magical", attackPower, self.numberOfAttacks, self.multiplier, self.splashMultiplier)
+    selfHarm = attackPower * 0.25 // 1
+    PrintText.Print_with_delay(f"You recieve recoil damage!")
+    player.damage_taken(selfHarm, "magical")
+    player.abilityPoints -= self.abilityCost
+
+class ArcaneFlood:
+  def __init__(self):
+    self.name = "Arcane Flood"
+    self.multiplier = 0.5
+    self.splashMultiplier = 0.5
+    self.numberOfAttacks = 3
+    self.abilityCost = 7
+  
+  def execute(self, encounter, player):
+    attackPower = player.intellect
+    AttackMethods.all_target_attack(self, encounter, self.name, "magical", attackPower, self.numberOfAttacks, self.multiplier, self.splashMultiplier)
+    player.abilityPoints -= self.abilityCost
+
+class Drain:
+  def __init__(self):
+    self.name = "Drain"
+    self.multiplier = 0.5
+    self.numberOfAttacks = 1
+    self.abilityCost = 10
+  
+  def execute(self, encounter, player):
+    attackPower = player.intellect
+    AttackMethods.all_target_attack(self, encounter, self.name, "magical", attackPower, self.numberOfAttacks, self.multiplier)
+    healAmount = attackPower * choice([0.05, 0.075, 0.1, 0.125, 0.15]) * len(encounter) // 1
+    PrintText.Print_with_delay(f"You steal {healAmount} health from enemies!")
+    player.health += healAmount
+    if player.health > player.maxHealth:
+      player.health = player.maxHealth
+    player.abilityPoints -= self.abilityCost
+
+class Erase:
+  def __init__(self):
+    self.name = "Erase"
+    self.multiplier = 5
+    self.splashMultiplier = 5
+    self.numberOfAttacks = 1
+    self.abilityCost = 15
+  
+  def execute(self, encounter, player):
+    attackPower = player.intellect
+    AttackMethods.all_target_attack(self, encounter, self.name, "magical", attackPower, self.numberOfAttacks, self.multiplier, self.splashMultiplier)
+    player.abilityPoints -= self.abilityCost
+
+class DoubleCleave:
+  def __init__(self):
+    self.name = "Double Cleave"
+    self.multiplier = 1.2
+    self.splashMultiplier = 0.6
+    self.numberOfAttacks = 2
+    self.abilityCost = 7
+
+  def execute(self, encounter, player):
+    attackPower = player.strength
+    AttackMethods.random_secondary_target_attack(self, encounter, self.name, "physical", attackPower, self.numberOfAttacks, self.multiplier, self.splashMultiplier)
+    player.abilityPoints -= self.abilityCost
+
+class BladeDance:
+  def __init__(self):
+    self.name = "Blade Dance"
+    self.multiplier = 0.4
+    self.abilityCost = 10
+
+  def execute(self, encounter, player):
+    attackPower = player.strength
+    numberOfAttacks = randint(3, 7)
+    AttackMethods.random_targets_attack(self, encounter, self.name, "physical", attackPower, numberOfAttacks, self.multiplier)
+    player.abilityPoints -= self.abilityCost
+
+class InvigoratingShout:
+  def __init__(self):
+    self.name = "Invigorating Shout"
+    self.multiplier = 0.2
+    self.splashMultiplier = 0.2
+    self.numberOfAttacks = 1
+    self.abilityCost = 5
+  
+  def execute(self, encounter, player):
+    attackPower = player.intellect
+    AttackMethods.all_target_attack(self, encounter, self.name, "physical", attackPower, self.numberOfAttacks, self.multiplier, self.splashMultiplier)
+    healAmount = attackPower * choice([0.05, 0.075, 0.1, 0.125, 0.15]) * len(encounter) // 1
+    PrintText.Print_with_delay(f"You restore {healAmount} magic!")
+    player.abilityPoints += healAmount
+    if player.abilityPoints > player.maxMagic:
+      player.abilityPoints = player.maxMagic
+
+class FuriousCleave:
+  def __init__(self):
+    self.name = "Furious Cleave"
+    self.multiplier = 2
+    self.splashMultiplier = 1.5
+    self.numberOfAttacks = 1
+    self.abilityCost = 7
+
+  def execute(self, encounter, player):
+    attackPower = player.strength
+    AttackMethods.random_secondary_target_attack(self, encounter, self.name, "physical", attackPower, self.numberOfAttacks, self.multiplier, self.splashMultiplier)
+    player.abilityPoints -= self.abilityCost
+
+class EnragedFlurry:
+  def __init__(self):
+    self.name = "Enraged Flurry"
+    self.multiplier = 0.5
+    self.abilityCost = 10
+
+  def execute(self, encounter, player):
+    attackPower = player.strength
+    numberOfAttacks = randint(5, 10)
+    AttackMethods.single_target_attack(self, encounter, self.name, "physical", attackPower, numberOfAttacks, self.multiplier)
+    selfHarm = attackPower * 0.05 * numberOfAttacks // 1
+    PrintText.Print_with_delay(f"You recieve recoil damage!")
+    player.damage_taken(selfHarm, "physical")
+    player.abilityPoints -= self.abilityCost
+
+class BerserkSlash:
+  def __init__(self):
+    self.name = "Berserk Slash"
+    self.multiplier = 5
+    self.numberOfAttacks = 1
+    self.abilityCost = 15
+
+  def execute(self, encounter, player):
+    attackPower = player.strength
+    AttackMethods.single_target_attack(self, encounter, self.name, "physical", attackPower, self.numberOfAttacks, self.multiplier)
+    selfHarm = attackPower * 0.5 // 1
+    PrintText.Print_with_delay(f"You recieve recoil damage!")
+    player.damage_taken(selfHarm, "physical")
+    player.abilityPoints -= self.abilityCost
