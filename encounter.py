ca35410197e321b1ad97160a952917b98eda4fb5
Kevin Levasseur <kevin_levasseur@telus.net>
2024-09-19 20:21:00 -0700

    feat: factor out choose ability and alter text prompts, won't let you choose an ability if you don't have the ap for it

--- encounter.py
+++ encounter.py
@@ -11,29 +11,37 @@ class Fight:
     self.targetIndex = None
     self.experienceEarned = 0
     self.jobPointsEarned = 0
+
+  def choose_ability(encounter, player):
+    abilities = []
+    for abilityName, abilityObject in player.abilities.items():
+      abilities.append(abilityName)
+      print(f"  {len(abilities)} - {abilityName} - {abilityObject.abilityCost} AP cost")
+    userInputAbility = int(input("Choose an ability -> ")) - 1
+    chosenAbility = player.abilities[abilities[userInputAbility]]
+    if chosenAbility.abilityCost > player.abilityPoints:
+      PrintText.Print_with_delay("You don't have enough ability points to use that\n")
+      Fight.choose_ability(encounter, player)
+    else:
+      player.abilities[abilities[userInputAbility]].execute(encounter, player)
     
   def battle(self, encounter: Enemy, player: Character):
     while encounter and player.health > 0:
-      PrintText.Print_with_delay(f"You have {player.health}/{player.maxHealth} health points.\n")
-      PrintText.Print_with_delay(f"You have {player.magic}/{player.maxMagic} magic points.\n\n")
-      print("Actions:\n  'f' - fight\n  'i' - inspect enemy\n  'r' - run away\n")
+      PrintText.Print_with_delay(f"You have {player.health}/{player.maxHealth} HP.\n")
+      PrintText.Print_with_delay(f"You have {player.abilityPoints}/{player.maxAbilityPoints} AP.\n\n")
+      print("Actions:\n  1 - fight\n  2 - inspect enemy\n  3 - run away\n")
       userInputAction = input("What do you do? -> ")
       ConsoleCommands.clear_console()
 
-      if userInputAction == "f":
-        abilities = []
-        for abilityName in player.abilities:
-          abilities.append(abilityName)
-          print(f"  {len(abilities)} - {abilityName}")
-        userInputAbility = int(input("Choose an ability -> ")) - 1
-        player.abilities[abilities[userInputAbility]].execute(encounter, player)
+      if userInputAction == "1":
+        Fight.choose_ability(encounter, player)
 
-      elif userInputAction == "i":
+      elif userInputAction == "2":
         Draw.inspect_enemy()
         for enemy in encounter:
           enemy.display_stats()
 
-      elif userInputAction == "r":
+      elif userInputAction == "3":
         Draw.escape()
         PrintText.Print_with_delay(f"You escaped!\n")
         break
